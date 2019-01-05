0d1c570946a0dd4d467e77440d3954e0b351a106
avalos010 <luizavalos40@gmail.com>
2019-01-05 15:29:47 -0600

    updated ReadMe

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ccxt from 'ccxt';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        CryptoExchange
+        </div>
+    )
+  }
+};
+
+export default App;
