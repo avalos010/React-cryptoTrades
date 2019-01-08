9a2254131b281f0654b97a5e269980cf9d20679e
avalos010 <luizavalos40@gmail.com>
2019-01-07 19:32:39 -0600

    Updates

---
+++ precache-manifest.da6ae8036f75a6330aaa3bf4e671a4b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6613b4f840c0d6b13ee5",
+    "url": "/React-cryptoTrades/static/js/main.6613b4f8.chunk.js"
+  },
+  {
+    "revision": "436108215b8d098501a1",
+    "url": "/React-cryptoTrades/static/js/1.43610821.chunk.js"
+  },
+  {
+    "revision": "53e08d72708a46d3d347",
+    "url": "/React-cryptoTrades/static/js/runtime~main.53e08d72.js"
+  },
+  {
+    "revision": "7a817cad46292430559bf1c15262e9cf",
+    "url": "/React-cryptoTrades/index.html"
+  }
+];
